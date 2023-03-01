9dadc8eb3af7b15ccdd429f68d63beaa12b1a19e
Martins Ndifon <martinsndifon@gmail.com>
2023-03-01 14:35:25 +0000

    Task 0 completed

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
-/*
- * binary_tree_node - create a binary tree node
- * @parent - pointer to the parent node of the node to create
- * @value - the value to put in the node
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: the value to put in the node
  *
  * Return: a pointer to the parent node
  */
