1eed3d42862f0dd90ebf87ffc077518dc5e24069
Martins Ndifon <martinsndifon@gmail.com>
2023-03-02 23:53:45 +0000

    Fixed error in file

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -5,6 +5,26 @@ int balance(const binary_tree_t *tree);
 avl_t *avl_insert_recursive(avl_t **tree, avl_t *parent,
 		avl_t **new, int value);
 avl_t *avl_insert(avl_t **tree, int value);
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: If tree is NULL, your function must return 0, else return height.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t l = 0, r = 0;
+
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
+}
 
 /**
  * height - Measures the height of a binary tree.
