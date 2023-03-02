e459b1533235465bf4e2d8bcfecadd21ad49bb82
Martins Ndifon <martinsndifon@gmail.com>
2023-03-02 22:49:55 +0000

    Main files and other fix

--- binary_trees.h
+++ binary_trees.h
@@ -85,5 +85,7 @@ heap_t *heap_insert(heap_t **root, int value)
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+binary_tree_t *basic_tree(void);
+void print_array(const int *array, size_t size);
 
 #endif
